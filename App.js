ead969178519bd3fc76ad1c1b2871119874550d0
Pablo Carro <pablo.carro@live.com>
2023-02-14 15:30:43 -0300

    reducer and store setup

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! And start coding!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! And start coding!</Text>
+      </View>
+    </Provider>
   );
 }
 
