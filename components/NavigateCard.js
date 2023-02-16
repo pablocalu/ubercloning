34fc55beb76237a7a2fcdddac85de95381e11d1d
Pablo Carro <pablo.carro@live.com>
2023-02-16 19:19:38 -0300

    added cars

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -44,7 +44,9 @@ const NavigateCard = () => {
                 <NavFavourites/>
             </View>
             <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
-                <TouchableOpacity style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+                <TouchableOpacity style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+                    onPress={()=> navigation.navigate("RideOptionsCard")}
+                >
                     <Icon name='car' type='font-awesome' color={'white'} size={16}/>
                     <Text style={tw`text-white text-center`}>
                         Rides
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,13 +1,64 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View, FlatList, Image} from 'react-native';
+import { Icon } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import tw from 'tailwind-react-native-classnames';
+
+const data = [
+    {
+        id: 'Uber-X-123',
+        title: 'UberX',
+        multiplier: 1,
+        image: 'https://links.papareact.com/3p'
+    },
+    {
+        id: 'Uber-XL-456',
+        title: 'Uber XL',
+        multiplier: 1.2,
+        image: 'https://links.papareact.com/5w8'
+    },
+    {
+        id: 'Uber-LUX-789',
+        title: 'Uber LUX',
+        multiplier: 1.75,
+        image: 'https://links.papareact.com/7pf'
+    }
+]
+
 
 const RideOptionsCard = () => {
+
+    const navigation = useNavigation()
     return (
-        <View>
-            <Text>
-                HOla
-            </Text>
-        </View>
+        <SafeAreaView style={tw`bg-white flex-grow`}>
+            <View>
+                <TouchableOpacity style={tw`absolute top-3 left-5 p-3 rounded-full`}
+                    onPress={()=> navigation.navigate('NavigateCard')}
+                >
+                    <Icon name='chevron-left' type='fontawesome'/>
+                </TouchableOpacity>
+                <Text style={tw`text-center py-5 text-xl`}>
+                    Select a Ride
+                </Text>
+            </View>
+            <FlatList
+                data={data}
+                keyExtractor={(item)=> item.id}
+                renderItem={({ item: { image, id, multiplier, title} }) => (
+                    <TouchableOpacity>
+                        <Image
+                            style={{
+                                width: 100,
+                                height: 100,
+                                resizeMode: 'contain'
+                            }}
+                            source={{uri: image}}
+                        />
+                    </TouchableOpacity>
+                )}
+            />
+        </SafeAreaView>
     );
 }
 
