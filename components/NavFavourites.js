a16aafb9bb0f904d7923dde52040ff88bd817a32
Pablo Carro <pablo.carro@live.com>
2023-02-16 18:36:22 -0300

    styles

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {View, StyleSheet, FlatList} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements';
+import tw from 'tailwind-react-native-classnames';
 
 const data = [
     {
@@ -19,7 +22,32 @@ const data = [
 const NavFavourites = () => {
     return (
         <FlatList
-        
+        data={data}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={()=> (
+            <View
+                style={[tw`bg-gray-200`, { height: 0.5 }]}
+            />
+        )}
+        renderItem={({item: { location, destination, icon}}) => (
+            <TouchableOpacity style={tw`flex-row items-center p-5 `}>
+                <Icon
+                    style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+                    name={icon}
+                    type='ionicon'
+                    color={'white'}
+                    size={18}
+                />
+                <View>
+                    <Text style={tw`font-semibold text-lg`}>
+                        {location}
+                    </Text>
+                    <Text style={tw`text-gray-500`}>
+                        {destination}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        )}    
         />
     );
 }
